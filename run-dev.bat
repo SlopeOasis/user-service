f9fb3103d2c966c50a7bfbfe1ab3c47469b0ff9c
AlesPozar <ales.pozar003@gmail.com>
2025-12-14 23:07:30 +0100

    lmao gitignore ni ignoru for some reason

--- run-dev.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-REM Runs user-service on port 8080 with Clerk JWT configuration - DEV MODE (no signature validation)
-set SERVER_PORT=8080
-set CLERK_ISSUER=https://upright-bird-25.clerk.accounts.dev
-set CLERK_JWKS_URL=https://upright-bird-25.clerk.accounts.dev/.well-known/jwks.json
-set JWT_DEV_MODE=true
-mvn spring-boot:run
