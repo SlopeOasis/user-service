48f2e32a777d9e8c7447f43c6871f18c53672537
AlesPozar <ales.pozar003@gmail.com>
2026-01-04 21:43:40 +0100

    dodan VM IP v CORS

--- src/main/java/com/slopeoasis/user/config/WebConfig.java
+++ src/main/java/com/slopeoasis/user/config/WebConfig.java
@@ -15,7 +15,9 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins(
                     "http://localhost:3000",
-                    "https://frontend-navy-iota-66.vercel.app"
+                    "https://frontend-navy-iota-66.vercel.app",
+                    "http://20.199.136.220:8080",
+                    "https://20.199.136.220:8080"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true)
